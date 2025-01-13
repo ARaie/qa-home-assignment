33271c2ac3030c6d79ce5cb5834ec4aa30e746c0
annik <annika_875@hotmail.com>
2025-01-13 21:02:09 +0200

    Docker corrections

--- CardValidation.Web/Program.cs
+++ CardValidation.Web/Program.cs
@@ -12,30 +12,26 @@ public class Program
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
 
-        // If running in Docker, listen on HTTP instead of redirecting to HTTPS
         if (app.Environment.IsDevelopment())
         {
             app.UseHttpsRedirection();
         }
         else
         {
-            // In Docker, disable HTTPS redirection for simplicity
-            app.UseRouting(); // Enable routing without HTTPS redirection
+            app.UseRouting(); 
         }
 
         app.UseAuthorization();
 
         app.MapControllers();
 
-        // Ensure the app listens on all IPs for Docker
-        app.Run("http://0.0.0.0:80"); // This binds the app to port 80 inside the container
+        app.Run("http://0.0.0.0:80"); 
     }
 
     public static void ConfigureServices(IServiceCollection services)
@@ -52,49 +48,3 @@ public class Program
         });
     }
 }
-
-
-/*using CardValidation.Core.Services;
-using CardValidation.Core.Services.Interfaces;
-using CardValidation.Infrustructure;
-
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        ConfigureServices(builder.Services);
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
-    }
-
-    public static void ConfigureServices(IServiceCollection services)
-    {
-        services.AddControllers();
-        services.AddEndpointsApiExplorer();
-        services.AddSwaggerGen();
-
-        services.AddTransient<ICardValidationService, CardValidationService>();
-
-        services.AddMvc(options =>
-        {
-            options.Filters.Add(typeof(CreditCardValidationFilter)); ;
-        });
-    }
-}*/
--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,19 @@
-# Kasutame SDK pilti, et ehitada ja testida rakendust
 FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
 
 WORKDIR /app
 
-# Kopeerige kogu projekt konteinerisse
 COPY . .
 
-# Taastame sõltuvused
 RUN dotnet restore
 
-# Ehita rakendus
 RUN dotnet build --configuration Release
 
-# Testige rakendust
 RUN dotnet test /app/CardValidation.Tests/ --logger "trx;LogFileName=test-results.trx"
 
-# Kasutame ainult runtime pilti, et käivitada rakendus
 FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS runtime
 
 WORKDIR /app
 
-# Kopeerige ehitusest failid
 COPY --from=build /app /app
 
 EXPOSE 5000
